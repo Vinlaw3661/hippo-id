afc5071d53d6b325dd02ed236d05c93034ef48b1
Vinlaw <vmudehwe3661@gmail.com>
2025-01-05 12:38:39 -0500

    Add verbosity to identify function

--- hippo.py
+++ hippo.py
@@ -3,8 +3,10 @@ import os
 import cv2
 import numpy as np 
 from utils.helpers import (
+    verbose_print,
     segment_faces,
     is_known_face,
+    is_known_face_deepface,
     ask_for_name,
     listen_for_name,
     acknowledge_person,
@@ -24,35 +26,46 @@ class Hippo:
         self.audio_save_directory = audio_save_directory
         self.face_states = [FaceState.UNDETECTED, FaceState.UNKNOWN]
 
-    def identify(self, img) -> tuple[np.ndarray, np.ndarray]:
-        print("---------------------------Starting Light Mode---------------------------------")
-        database_path = "database/faces"
-        print(f"\nUsing database path: {database_path}")
-        print("\nSegmenting faces...")
-        segmented_face , face_path, face_state = segment_faces(self.img)
-        if face_state == FaceState.UNDETECTED:
-            print("No faces detected in the image")
-            return False, FaceState.UNDETECTED
-        print("\nSegmentation Done!")
-        single_face, single_face_path = segmented_face, face_path
-        print(f"\nIdentifying person in {single_face_path}...")
+    def identify(self, image, verbose: bool = True) -> tuple[np.ndarray, np.ndarray]:
+        
+        verbose_print("---------------------------Starting Light Mode---------------------------------")
+        verbose_print(f"\nUsing database path: {self.database_path}")
+        verbose_print("\nSegmenting faces...")
 
-        is_known, possible_name = is_known_face(single_face_path, database_path)
+        segmented_face , face_path, face_state = segment_faces(image)
+
+        if face_state == FaceState.UNDETECTED:
+            verbose_print("No faces detected in the image")
+            return False, FaceState.UNDETECTED
+        
+        verbose_print("\nSegmentation Done!")
+
+        single_face, single_face_path = segmented_face, face_path
+        verbose_print(f"\nIdentifying person in {single_face_path}...")
+
+        if self.storage_mode == StorageMode.CHROMA:
+            is_known, possible_name = is_known_face(single_face_path, self.database_path)
+
+        elif self.storage_mode == StorageMode.DEEPFACE:
+            is_known, possible_name = is_known_face_deepface(single_face_path, self.database_path)
+
         if is_known:
-            print(f"\nPerson already known! :{possible_name}")
+            verbose_print(f"\nPerson already known! :{possible_name}")
             return False, possible_name
-        print("\nAsking for person's name")
+        
+        verbose_print("\nAsking for person's name")
         name_asked = ask_for_name(single_face_path, self.use_elevenlabs)
-        print("\nAsking Done!")
+        verbose_print("\nAsking Done!")
+
         if name_asked:
-            print(f"\nListening for name...")
+            verbose_print(f"\nListening for name...")
             name = listen_for_name(self.use_assemblyai, self.audio_path, self.audio_save_directory)
-            print(f"\nName captured as: {name}")
-            identity_path = f"{database_path}/{name.lower()}/{name.lower()}.png"
-            print(f"\nSaving identity at: {identity_path}")
+            verbose_print(f"\nName captured as: {name}")
+            identity_path = f"{self.database_path}/{name.lower()}/{name.lower()}.png"
+            verbose_print(f"\nSaving identity at: {identity_path}")
             os.makedirs(os.path.dirname(identity_path), exist_ok=True)
             cv2.imwrite(identity_path, single_face)
-            print("\nIdentity saved!")
+            verbose_print("\nIdentity saved!")
             acknowledge_person(name)
             return True, name
         
